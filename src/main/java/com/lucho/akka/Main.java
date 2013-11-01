df15505f575bb8da0542c67c438a23f5628882fb
Luciano Leggieri <lleggieri@despegar.com>
2013-11-01 18:19:32 -0300

    playing witk akka java

--- src/main/java/com/lucho/akka/Main.java
+++ src/main/java/com/lucho/akka/Main.java
@@ -1,12 +1,67 @@
 package com.lucho.akka;
 
+import akka.actor.ActorRef;
 import akka.actor.ActorSystem;
+import akka.actor.Props;
+import akka.dispatch.OnComplete;
+import akka.japi.Util;
+import akka.pattern.Patterns;
+
+import java.util.concurrent.CompletableFuture;
+import java.util.concurrent.Executor;
+import java.util.concurrent.TimeUnit;
+import java.util.function.BiConsumer;
 
 public class Main {
 
-    public static void main(String[] args) {
+    public static final class CoolCompletion<T> extends OnComplete<T> {
+
+        private final BiConsumer<Throwable, T> completer;
+
+        public CoolCompletion(final BiConsumer<Throwable, T> completer) {
+            this.completer = completer;
+        }
+
+        @Override
+        public final void onComplete(Throwable failure, T success) throws Throwable {
+            completer.accept(failure, success);
+        }
+    }
+
+    public static final class FutureCompletion<T> extends OnComplete<T> {
+
+        private final CompletableFuture<T> completer;
+
+        public FutureCompletion(final CompletableFuture<T> completer) {
+            this.completer = completer;
+        }
+
+        @Override
+        public final void onComplete(Throwable failure, T success) throws Throwable {
+            if (failure == null) {
+                completer.complete(success);
+            } else {
+                completer.completeExceptionally(failure);
+            }
+        }
+    }
+
+    public static void main(String[] args) throws Exception {
+        //BiPredicate
         ActorSystem actorSystem = ActorSystem.create();
-        actorSystem.actorOf()
-        actorSystem.shutdown();
+        try {
+            ActorRef pinger = actorSystem.actorOf(Props.create(PingerActor.class));
+            scala.concurrent.Future<Object> future = Patterns.ask(pinger, Message.PING, TimeUnit.MINUTES.toMillis(1));
+            CompletableFuture<Message> javaFuture = new CompletableFuture<>();
+
+            future.mapTo(Util.classTag(Message.class)).onComplete(new FutureCompletion<Message>(javaFuture), actorSystem.dispatcher());
+
+            //javaFuture.thenAccept(System.out::println);
+            javaFuture.thenAcceptAsync(System.out::println, (Executor) actorSystem.dispatcher());
+
+            javaFuture.get();
+        } finally {
+            actorSystem.shutdown();
+        }
     }
 }
